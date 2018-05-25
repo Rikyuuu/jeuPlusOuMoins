8971a999d5301419b971c75e3d55b05b86707d7c
Rikyuu <julien.delissenne@gmail.com>
2018-05-25 10:32:09 +0200

    Add class 'hidden' in the css, add id on button's, remove class hidden in startGame.js with a function's for hidden or visible the div (when the user click on the button), Function startGame changed and renamed to 'levelChoice', moving statements in the startGame function to the selectChoice function, adding statements in the startGame function.

--- js/startGame.js
+++ js/startGame.js
@@ -1,7 +1,28 @@
+// Déclarations des variables
 var start = document.getElementById('start');
-start.addEventListener('click', startGame);
+var validerSelectLevelButton = document.getElementById('validerSelectLevelChoice');
+var validerGameWindowButton = document.getElementById('validerGameWindow');
 
+var selectLevelWindow = document.querySelector('.selectLevelWindow');
+
+// Lancement des fonctions à partir des boutons
+start.addEventListener('click', startGame);
+validerSelectLevelButton.addEventListener('click', levelChoice);
+// validerGameWindowButton.addEventListener('click', );
+
+
+// Fonction lancement du jeu
 function startGame () {
+    start.setAttribute("class", "hidden");
+    selectLevelWindow.classList.remove("hidden");
+
+}
+
+// Fonction choix des niveaux de difficultés
+function levelChoice () {
+    validerSelectLevelButton.setAttribute("class", "hidden");
+    divSelectLevelWindow.classList.remove("hidden");
+
     var radios = document.getElementsByName('lvl');
     var valeur;
     for(var i = 0; i < boutons.length; i++){
@@ -11,3 +32,7 @@ function startGame () {
     }
     alert(valeur);
 }
+
+
+
+// pour remove les class hidden ELEMENT.classList.remove("CLASS_NAME");
