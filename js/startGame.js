0fb5659c6dd0853cd01ce327f8011696c9821e99
Rikyuu <julien.delissenne@gmail.com>
2018-05-27 18:45:34 +0200

    adding two variables for display the level chosen by the user, the first variable for retrieving the text according to the selected, the second for showing the difficulty level in index.html. Adding a variable in function levelChoice (), the variable is equal to the level of difficulty selected. Added several conditions that check the level of difficulty chosen to allow the display with innerHTML (still in the same function) in the index.html. Added several comments and English translation of other comments.

--- js/startGame.js
+++ js/startGame.js
@@ -4,33 +4,39 @@ var startWindow = document.getElementById('startWindow');
 var gameWindow = document.getElementById('gameWindow');
 
 
-// Variables pour afficher le niveau de difficulté choisi dans index.html
-var showDifficultLevel = document.getElementById('showDifficultLevel');
-var showLevelWindow = document.getElementsByName('lvl');
+// Variables to display the difficulty level chosen in index.html
+var showLevelWindow = document.getElementById('selectLevelChoice'); // To retrieve the text according to the selected
+var showDifficultLevel = document.getElementById('showDifficultLevel'); // for show the difficult level in index.html
 
 var startButton = document.getElementById('startButton');
 var validateSelectLevelButton = document.getElementById('validateSelectLevelChoice');
-/*var validateGameWindowButton = document.getElementById('validerGameWindow');*/
+// var validateGameWindowButton = document.getElementById('validerGameWindow');
 
 var radios = document.getElementsByName('lvl');
 var levelValue;
+// End variable declaration
 
-// Launching functions from the buttons
+// Event launching functions from the buttons
 startButton.addEventListener('click', startGame);
 validateSelectLevelButton.addEventListener('click', levelChoice);
 // validateGameWindowButton.addEventListener('click', );
+// End event lauching functions
+
+
+// Functions
 
 // Launch function of the game
 function startGame () {
     startButton.setAttribute("class", "hidden");
     selectLevelWindow.classList.remove("hidden");
-
-    // Pour afficher le level choisi par l'utilisateur
-    showDifficultLevel.innerHTML = showLevelWindow[0].innerText;
 }
 
 // Function choice of difficulty levels
 function levelChoice () {
+    
+    // Declaration of a variable equal to the level of difficulty selected
+    var selectedShowLevelWindow = showLevelWindow.selectedIndex;
+
     selectLevelWindow.setAttribute("class", "hidden");
     gameWindow.classList.remove("hidden");
 
@@ -39,7 +45,29 @@ function levelChoice () {
             levelValue = radios[i].value;
         }
     }
+
+    // The value compared is a value of the nodeList selected (example: [0]) so 0 is the level 1
+    if(selectedShowLevelWindow == '0') {
+        // To display the level chosen by the user
+        showDifficultLevel.innerHTML = 'Niveau 1 (nombre entre 0 et 10)';
+    }
+    else if(selectedShowLevelWindow == '1') {
+        // To display the level chosen by the user
+        showDifficultLevel.innerHTML = 'Niveau 2 (nombre entre 0 et 20)';
+    }
+    else if(selectedShowLevelWindow == '2') {
+        // To display the level chosen by the user
+        showDifficultLevel.innerHTML = 'Niveau 3 (nombre entre 0 et 30)';
+    }
+    else if(selectedShowLevelWindow == '3') {
+        // To display the level chosen by the user
+        showDifficultLevel.innerHTML = 'Niveau challenger (nombre entre 0 et 1000 avec une seule chance)';
+    }
+    else {
+        showDifficultLevel.innerHTML = 'Erreur lors du choix du niveau'
+    }
+
+    // Launch function game()
     game();
 }
-
-// for remove the hidden class  "ELEMENT.classList.remove("CLASS_NAME");"
+// End functions
